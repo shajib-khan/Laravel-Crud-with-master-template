e286a2688199c5381ed6ffcfb75c937674d36685
shajib <shajibkhan946@gmail.com>
2024-02-14 11:43:02 +0600

    little design change

--- resources/views/show_data.blade.php
+++ resources/views/show_data.blade.php
@@ -8,11 +8,12 @@
         @endif
         <table class="table table-bordered">
             <thead>
-              <tr">
+              <tr>
                 <th scope="col">#</th>
                 <th scope="col">Email</th>
                 <th scope="col">Name</th>
-                <th scope="col">Action</th>
+                <th scope="col">Customize</th>
+                <th scope="col">Remove</th>
               </tr>
             </thead>
             <tbody>
